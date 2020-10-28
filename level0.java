f43e5fe0bc3be339034a530741e9ad9b1ba4d1a4
holo <71625361+holo001@users.noreply.github.com>
2020-10-28 13:13:01 +0800

    Add files via upload

---
+++ level0.java
@@ -0,0 +1,13 @@
+public class learning {
+    public static void main(String[]args){
+        int n = 1;
+        while(n <= 100){
+            System.out.println(n);
+            n = n + 1;
+            if (n == 50){
+                n = n + 1;
+            }
+        }
+
+    }
+}
